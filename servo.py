6e3cc16e4b1d65315f62fc5ee5c203ab1b9c71c4
Jonas <jonasrydenhag@users.noreply.github.com>
2015-11-15 18:59:50 +0900

    Adds entry to the db on every servo switch

--- servo.py
+++ servo.py
@@ -1,7 +1,27 @@
 #!/bin/bash
-
+import pymongo
 import RPi.GPIO as GPIO
 import time
+import datetime
+import sys
+from pymongo import MongoClient
+
+client = MongoClient('localhost', 27017)
+db = client['temperature-station']
+switches = db.switches
+
+newSwitch = {
+ 	"date": datetime.datetime.utcnow()
+}
+
+for switch in switches.find().limit(1).sort("date", -1):
+	if switch['state'] == 'on':
+		newSwitch['state'] = 'off'
+	elif switch['state'] == 'off':
+		newSwitch['state'] = 'on'
+	else:
+		GPIO.cleanup()
+		sys.exit()
 
 pinNr = 37
 
@@ -18,3 +38,5 @@ time.sleep(0.25)
 p.stop()
 
 GPIO.cleanup()
+
+switches.insert_one(newSwitch)
