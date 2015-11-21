e56445b677e5b960d5fe987d9eb2735643f5f16e
Jonas <jonasrydenhag@users.noreply.github.com>
2015-11-21 16:06:43 +0900

    Saves temp readings to db

--- temp.py
+++ temp.py
@@ -1,7 +1,32 @@
 #!/usr/bin/python
 
 import Adafruit_DHT as dht
+import time
+import pymongo
+import datetime
+from pymongo import MongoClient
 
-h,t = dht.read_retry(dht.DHT22, 19)
-print 'Temp={0:0.1f}*C Humidity={1:0.1f}%'.format(t,h)
+client = MongoClient('localhost', 27017)
+db = client['temperature-station']
+readings = db.readings
 
+reading = 0
+
+def read():
+	global reading
+	global readings
+
+	if reading == 1:
+		time.sleep(3)
+
+	h,t =  dht.read_retry(dht.DHT22, 19)
+
+	readings.insert_one({
+		"date": datetime.datetime.utcnow(),
+		"temperature": t,
+		"humidity" : h
+	})
+
+	reading = 0
+
+	return (h, t)
