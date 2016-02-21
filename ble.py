eecf2bf858e020362d45d0d298530b598710582e
Jonas <jonasrydenhag@users.noreply.github.com>
2016-02-21 20:54:57 +0900

    Working copy of led with ble

--- ble.py
+++ ble.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python
+import sys
+sys.path.insert(0, '/home/pi/bluepy/bluepy')
 
+import temp
 import btle
 import struct
 from btle import UUID
@@ -11,16 +14,28 @@ class MyDelegate(btle.DefaultDelegate):
 
     def handleNotification(self, cHandle, data):
         if data == "temp":
-            svc = p.getServiceByUUID(UUID("bd011f22-7d3c-0db6-e441-55873d44ef40"))
-            ch = svc.getCharacteristics(UUID("0503b819-c75b-ba9b-3641-6a7f338dd9bd"))[0]
-            ch.write(struct.pack("B", 0x02))
-            ch.write(struct.pack("B", 0x01))
-            ch.write(struct.pack("B", 0x05))
-            ch.write(struct.pack("B", 0x04))
+            self.writeTemp()
             print "yeah baby!"
 
-def _TI_UUID(val):
-    return UUID("%08X-0451-4000-b000-000000000000" % (0xF0000000+val))
+    def writeTemp(self):
+        svc = p.getServiceByUUID(UUID("bd011f22-7d3c-0db6-e441-55873d44ef40"))
+        ch = svc.getCharacteristics(UUID("0503b819-c75b-ba9b-3641-6a7f338dd9bd"))[0]
+
+        for char in self.getTemp():
+            ch.write(struct.pack("B", int(char)))
+
+    def getTemp(self):
+        h,t,date = temp.read()
+
+        orgValue = round(t, 2)
+        orgStringValue = str(orgValue)
+        cleanStringValue = orgStringValue.replace('.', '')
+
+        if len(cleanStringValue) < 4:
+            cleanStringValue += "0"
+
+        return list(cleanStringValue)
+
 
 # Initialisation  -------
 
@@ -29,7 +44,6 @@ p.setDelegate(MyDelegate())
 
 
 # Setup to turn notifications on, e.g.
-
 
 # Main loop --------
 
